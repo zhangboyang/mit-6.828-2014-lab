7ffd8183526fbb1dc994d2b3ace72dfbee088ca1
Zhang Boyang <zhangboyang.id@gmail.com>
2016-02-05 22:50:50 +0800

    exercise 12. improve stack backtrace
    also fixed bug in exercise 11

--- kern/monitor.c
+++ kern/monitor.c
@@ -24,6 +24,7 @@ struct Command {
 static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
+	{ "backtrace", "Print the stack backtrace", mon_backtrace },
 };
 #define NCOMMANDS (sizeof(commands)/sizeof(commands[0]))
 
@@ -60,9 +61,17 @@ mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
 	cprintf("Stack backtrace:\n");
 	uint32_t *ebp = (uint32_t *) read_ebp();
+	uint32_t eip;
+	struct Eipdebuginfo info;
 	while (ebp) {
 	    cprintf("ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\n",
-	        ebp[0], ebp[1], ebp[2], ebp[3], ebp[4], ebp[5], ebp[6]);
+	        ebp, ebp[1], ebp[2], ebp[3], ebp[4], ebp[5], ebp[6]);
+	    eip = ebp[1];
+	    debuginfo_eip(eip, &info);
+	    cprintf("         %s:%d: %.*s+%d\n",
+	        info.eip_file, info.eip_line, info.eip_fn_namelen, info.eip_fn_name,
+	        eip - info.eip_fn_addr);
+	    
 	    ebp = (uint32_t *) *ebp;
 	}
 	return 0;
